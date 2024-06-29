caa54c28c640041813b7703a850f3d09f53d5318
kartik294 <kartikgeu@gmail.com>
2024-06-29 14:44:45 +0530

    third commit

--- src/app/page.js
+++ src/app/page.js
@@ -19,9 +19,9 @@ const Blog = () => {
           height: 100%;
         }
       `}</style>
-      <div className="flex items-center justify-center w-full bg-amber-400 h-full">
-        <div className="bg-white flex flex-col border border-gray-950 mt-15 p-6 w-96 mx-8 rounded-2xl relative">
-          <div className="mx-10 mt-10">
+      <div className="flex items-center justify-center w-full bg-amber-400 min-h-screen">
+        <div className="bg-white flex flex-col border border-gray-950 mt-10 p-6 w-full max-w-md mx-4 rounded-2xl relative">
+          <div className="mx-auto mt-10">
             <Image
               src="/images/article.svg"
               width={300}
@@ -30,14 +30,14 @@ const Blog = () => {
               className="rounded-2xl"
             />
           </div>
-          <span className="block w-min bg-yellow-400 text-center font-semibold px-4 py-2 mt-6 ml-10 rounded-lg">
+          <span className="block bg-yellow-400 text-center font-semibold px-4 py-2 mt-6 mx-auto rounded-lg">
             Learning
           </span>
-          <p className="pt-2 px-10">Published 21 Dec 2023</p>
-          <p className="font-black text-2xl mt-4 px-10">
+          <p className="pt-2 text-center">Published 21 Dec 2023</p>
+          <p className="font-black text-xl sm:text-2xl mt-4 text-center whitespace-nowrap">
             HTML & CSS foundations
           </p>
-          <p className="pt-4 px-6 text-base text-gray-400">
+          <p className="pt-4 text-base text-gray-400 text-center">
             These languages are the backbone of every
             <br />
             website, defining structure, content, and
@@ -45,13 +45,13 @@ const Blog = () => {
             presentation.
           </p>
 
-          <div className="mx-10 mt-12 flex flex-row">
+          <div className="flex justify-center items-center mt-12">
             <Image
               src="/images/avatar.jpg"
               width={40}
               height={40}
               alt="Avatar Image"
-              className="rounded-2xl pb-10"
+              className="rounded-full"
             />
             <p className="font-bold align-middle mt-2 ml-2">Greg Hooper</p>
           </div>
